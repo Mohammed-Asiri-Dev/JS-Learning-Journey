96a152c1f31756ba18d6ac4f2be964de4bb2c10f
Mohammed-Asiri-Dev <aboksa94@gmail.com>
2026-03-29 13:28:11 +0300

    Update console log messages for analyzeNumbers

--- analyze-numbers.js
+++ analyze-numbers.js
@@ -15,5 +15,5 @@ const analyzeNumbers = (...numbers) => {
 };
 
 // 4. تجربة الدالة وعرض النتائج في الكونسول
-console.log(analyzeNumbers(10, 20, 30, 50)); // سيطبع رسالة المجموع الكبير
-console.log(analyzeNumbers(5, 10, 15));      // سيطبع رسالة التشجيع
+console.log(analyzeNumbers(10, 20, 30, 50)); //سيطبع Total is 110: That's a large sum!
+console.log(analyzeNumbers(5, 10, 15));     //سيطبع Total is 30: Keep adding more numbers.
